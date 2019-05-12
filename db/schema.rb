5deaeb433a2a434a0ab869f34ed28e3e4b3385eb
rubencattan <rubencattan5@gmail.com>
2019-05-12 16:36:07 +0200

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_12_121711) do
+ActiveRecord::Schema.define(version: 2019_05_12_143000) do
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
     t.integer "arrondissement"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
